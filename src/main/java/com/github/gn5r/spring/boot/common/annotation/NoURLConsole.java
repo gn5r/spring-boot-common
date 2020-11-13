92745ce09f8f2eee8aa291a829eb93520143b137
gn5r <shangyuan.tuolang@gmail.com>
2020-11-13 23:06:27 +0900

    クラス名を統一させるために修正

--- src/main/java/com/github/gn5r/spring/boot/common/annotation/NoURLConsole.java
+++ src/main/java/com/github/gn5r/spring/boot/common/annotation/NoURLConsole.java
@@ -13,6 +13,6 @@ import java.lang.annotation.Target;
  */
 @Target({ ElementType.TYPE, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
-public @interface NoConsoleURL {
+public @interface NoURLConsole {
 
 }
