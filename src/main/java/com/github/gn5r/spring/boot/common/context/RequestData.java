318cb8afdc98397f8e7d4f25fd32bed68832d885
gn5r <shangyuan.tuolang@gmail.com>
2020-11-28 12:13:14 +0900

    final廃止。継承可能に修正。getメソッドをコメント付きで作成

--- src/main/java/com/github/gn5r/spring/boot/common/context/RequestData.java
+++ src/main/java/com/github/gn5r/spring/boot/common/context/RequestData.java
@@ -1,16 +1,63 @@
 package com.github.gn5r.spring.boot.common.context;
 
+import javax.servlet.http.HttpServletRequest;
+
+import lombok.Setter;
+
 /**
  * 各コントローラーメソッドに対するリクエストデータを格納するオブジェクト
  * 
  * @author gn5r
  * @since 0.5.0
  */
-@lombok.Data
-public final class RequestData {
+public class RequestData {
 
+    @Setter
     private String url;
+
+    @Setter
     private String subDirectory;
+
+    @Setter
     private String contextPath;
+
+    @Setter
     private String queryString;
+
+    /**
+     * リクエスト先URLを取得する
+     * 
+     * @return url
+     */
+    public String getUrl() {
+        return this.url;
+    }
+
+    /**
+     * リクエスト先の各コントローラーで記述したURL(ServletPath)を取得する
+     * 
+     * @return servletPath
+     * @see HttpServletRequest#getServletPath()
+     */
+    public String getSubDirectory() {
+        return this.subDirectory;
+    }
+
+    /**
+     * リクエスト先の各アプリケーションのコンテキストパスを取得する
+     * 
+     * @return コンテキストパス
+     */
+    public String getContextPath() {
+        return this.contextPath;
+    }
+
+    /**
+     * リクエスト先クエリを取得する
+     * 
+     * @return クエリ
+     */
+    public String getQueryString() {
+        return this.queryString;
+    }
 }
